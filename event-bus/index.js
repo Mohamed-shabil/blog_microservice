0ef0a9cd3f3ff142118c1769c4e1cae12c9c6b41
Mohamed Shabil <mohamedshabil231@gmail.com>
2023-12-07 22:22:51 +0530

    feat(event-bus) : added cache storage to store data

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,25 @@ const axios = require("axios");
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const events = [];
+
+
 app.post("/events", async (req, res) => {
   let event = req.body;
-  console.log(event);
+  events.push(event);
   await axios.post("http://localhost:4000/events", event);
   await axios.post("http://localhost:4001/events", event);
-  await axios.post("http://localhost:4002/events", event);
+  await axios.post("http://localhost:4002/events", event).catch((e)=>{console.log(e)})
   await axios.post("http://localhost:4003/events", event);
    
   res.send({ status: "OK" });
 });
+
+
+app.get('/events',(req,res)=>{
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log("Listening on Port 4005");
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+const axios = require('axios');
 const cors = require("cors");
 
 app.use(bodyParser.json());
@@ -7,11 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type , data) =>{
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -31,9 +28,23 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+}
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type,data);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on port 4002");
+  const res = await axios.get('http://localhost:4005/events');
+  for(let event of res.data){
+    console.log('Processing event :',event.type);
+    handleEvent(event.type,event.data);
+  }
 });
